8b8cdb74096119e0312a878a0ad619afad5f01c7
lsm-k <tmdals9482@gmail.com>
2025-03-19 10:21:52 +0900

    20250319-과제

--- 20251273이승민0319-2.py
+++ 20251273이승민0319-2.py
@@ -1,5 +1,6 @@
-#문제1-두개의 값을 입력받아 사각형과 삼각형 넓이 출력
 '''
+#문제1-두개의 값을 입력받아 사각형과 삼각형 넓이 출력
+
 w = int(input("밑변 길이 입력?"))
 h = int(input("높이 길이 입력?"))
 
@@ -8,10 +9,23 @@ triangle_area = w * h / 2
 
 print("사각형의 넓이 : ", square_area)
 print("삼각형의 넓이 : ", triangle_area)
-'''
+
 #문제2-원의 넓이 출력
 
 pi = 3.14
 radius = float(input("반지름 입력?"))
 circle_area = pi * radius * radius
 print("반지름이 ", radius, "인 원의 넓이 : %.3f"%circle_area)
+'''
+
+#점수 3개를 입력받아 총점과 평균 구해주세요
+
+score_1 = float(input("점수 1 입력"))
+score_2 = float(input("점수 2 입력"))
+score_3 = float(input("점수 3 입력"))
+
+total = score_1 + score_2 + score_3
+average = total / 3
+
+print("총점 : %.2f"%total)
+print("평균 : %.2f"%average)
